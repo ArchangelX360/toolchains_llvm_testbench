91a6147c96496ca00d060436baeac05a92ab7f37
Titouan Bion <titouan.bion@jetbrains.com>
2026-02-03 23:48:10 +0100

    Fix path to platforms/libc

--- build.cmd
+++ build.cmd
@@ -5,23 +5,23 @@ GOTO :CMDSCRIPT
 
 set -eux
 
-bazel build //cc/... --platforms=//native/platforms:linux-x86_64-glibc || echo "failed on linux-x86_64 (cc)"
-bazel build //cc/... --platforms=//native/platforms:linux-aarch64-glibc || echo "failed on linux-aarch64 (cc)"
-bazel build //cc/... --platforms=//native/platforms:macos-x86_64 || echo "failed on macos-x86_64 (cc)"
-bazel build //cc/... --platforms=//native/platforms:macos-aarch64 || echo "failed on macos-aarch64 (cc)"
-bazel build //cc/... --platforms=//native/platforms:windows-x86_64 || echo "failed on windows-x86_64 (cc)"
-bazel build //cc/... --platforms=//native/platforms:windows-aarch64 || echo "failed on windows-aarch64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:linux-x86_64-glibc || echo "failed on linux-x86_64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:linux-aarch64-glibc || echo "failed on linux-aarch64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:macos-x86_64 || echo "failed on macos-x86_64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:macos-aarch64 || echo "failed on macos-aarch64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:windows-x86_64 || echo "failed on windows-x86_64 (cc)"
+bazel build //cc/... --platforms=//build/native/platforms:windows-aarch64 || echo "failed on windows-aarch64 (cc)"
 bazel build //rust/... --keep_going
 exit 0
 :CMDSCRIPT
 
 call bazelisk.exe build //rust/... --keep_going
-echo "linux-x86_64-glibc (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:linux-x86_64-glibc
-echo "linux-aarch64-glibc (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:linux-aarch64-glibc
-echo "macos-x86_64 (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:macos-x86_64
-echo "macos-aarch64 (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:macos-aarch64
-echo "windows-x86_64 (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:windows-x86_64
-echo "windows-aarch64 (cc)" && call bazelisk.exe build //cc/... --platforms=//native/platforms:windows-aarch64
+echo "linux-x86_64-glibc (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:linux-x86_64-glibc
+echo "linux-aarch64-glibc (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:linux-aarch64-glibc
+echo "macos-x86_64 (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:macos-x86_64
+echo "macos-aarch64 (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:macos-aarch64
+echo "windows-x86_64 (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:windows-x86_64
+echo "windows-aarch64 (cc)" && call bazelisk.exe build //cc/... --platforms=//build/native/platforms:windows-aarch64
 
 set _exit_code=%ERRORLEVEL%
 popd

5c2ace62922add3703112748ecebc6fed88aaa2d
Titouan Bion <titouan.bion@jetbrains.com>
2026-02-11 18:15:33 +0100

    Make the main easier to test

--- rust/with-cc-build/src/main.rs
+++ rust/with-cc-build/src/main.rs
@@ -4,10 +4,10 @@ use std::path::PathBuf;
 use tar::Archive;
 
 fn main() {
-    let source_filepath = PathBuf::from("/tmp/hello.tar.zst");
+    let source_filepath = PathBuf::from("hello.tar.zst");
     let source_file = File::open(&source_filepath).unwrap();
     let reader = BufReader::new(source_file);
     let archive_reader = zstd::stream::Decoder::new(reader).unwrap();
-    let extraction_directory = PathBuf::from("/tmp/hello");
+    let extraction_directory = PathBuf::from("hello");
     Archive::new(archive_reader).unpack(extraction_directory).unwrap();
 }
